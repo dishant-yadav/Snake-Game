d88c38021d49081dacf93addd3c4fdb48a9bc5c3
“[Dishant <[dishantyadav599@gmail.com]>
2022-04-23 20:04:54 +0530

    added music functionality

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
 // game constants and variables
 let inputDirection = { x: 0, y: 0 };
-const foodSound = new Audio("/music/food.mp3");
-const gameOverSound = new Audio("/music/gameover.mp3");
-const moveSound = new Audio("/music/move.mp3");
-const musicSound = new Audio("/music/music.mp3");
+const foodSound = new Audio("./music/food.mp3");
+const gameOverSound = new Audio("./music/gameover.mp3");
+const moveSound = new Audio("./music/move.mp3");
+const musicSound = new Audio("./music/music.mp3");
 
 let score = 0;
 let speed = 7;
